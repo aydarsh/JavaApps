d4584eb88a12a4f731fe6e81259c15a20602c480
Aydar Sharafiev <aydarsh@gmail.com>
2017-03-01 23:21:35 +0300

    A simple QuadraticEquationSolver application

---
+++ QuadraticEquationSolver.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+class QuadraticEquationSolver {
+	public static void main(String[] args) {
+		QuadraticEquation quadraticEquation = new QuadraticEquation(1, 3, 1);
+		// System.out.println((quadraticEquation.findDiscriminant()).toString());
+		System.out.println((quadraticEquation.findRoots().toString()));
+	}
+}
+
+class QuadraticEquation {
+	private Double a, b, c;
+	
+	QuadraticEquation(double a, double b, double c){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public Double findDiscriminant(){
+		return b*b - 4*a*c;
+	}
+		
+	public ArrayList findRoots(){
+		ArrayList alRoots = new ArrayList();
+		Double discriminant = findDiscriminant();
+		Double x1, x2;
+
+		if (Math.abs(discriminant) < 2 * Double.MIN_VALUE) {
+			x1 = (-b + Math.sqrt(findDiscriminant())) / (2*a);
+			alRoots.add(x1);	
+		} else if (discriminant.compareTo(new Double("0.0")) > 0) {
+			x1 = (-b + Math.sqrt(findDiscriminant())) / (2*a);
+			x2 = (-b - Math.sqrt(findDiscriminant())) / (2*a);
+			alRoots.add(x1);
+			alRoots.add(x2);
+		}
+		return alRoots;
+	}	
+		
+}
